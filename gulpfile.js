8c495943f5c4265c6812bba7fb517699fc86826a
fishYu <kingfish86@163.com>
2016-06-22 22:29:48 +0800

    修改发布的gulp脚本

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,27 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var del = require("del");
+var gulpSequence = require('gulp-sequence');
+var replace = require('gulp-replace');
+var requirejsOptimize = require('gulp-requirejs-optimize');
+var htmlmin = require('gulp-htmlmin');
+var destDir = "dist";
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['css']);
+
+gulp.task('css', function() {
+    gulp.watch("src/scss/**/**", ['sass']);
+});
 
 gulp.task('sass', function(done) {
   gulp.src('./src/scss/base.scss')
     .pipe(sass())
+    .pipe(cleanCSS())
     .pipe(rename({ extname: '.min.css' }))
     .pipe(minifyCss({
           keepSpecialComments: 0
@@ -17,6 +29,59 @@ gulp.task('sass', function(done) {
     .pipe(gulp.dest('./src/www/css/'))
     .on('end', done);
 });
+
+gulp.task('release', gulpSequence('clean', ['_release:html','_release:css', '_release:js', '_copyFiles']));
+
+gulp.task('clean', function (done) {
+    del([destDir]).then(function () {
+        done();
+    });
+});
+gulp.task('_copyFiles', ['_copyImg', '_copyCss', '_copyJs'], function () {
+});
+
+gulp.task('_copyImg', function () {
+    return gulp.src(["src/www/images?([1])/**"])
+        .pipe(gulp.dest(destDir));
+});
+gulp.task('_copyCss', function () {
+    return gulp.src(["src/www/css/**"])
+        .pipe(gulp.dest(destDir + "/css/"));
+});
+
+gulp.task('_copyJs', function () {
+    return gulp.src(["src/www/js/vendor/require.js"], {base: 'src/www/js'})
+        .pipe(gulp.dest(destDir + "/js"));
+});
+
+gulp.task('_release:html', function () {
+    return gulp.src("src/www/index.html")
+        .pipe(htmlmin({collapseWhitespace: true}))
+//        .pipe(replace('<script src="js/debug.js"></script>', ''))
+        .pipe(rename({basename: 'index'}))
+        .pipe(gulp.dest(destDir))
+});
+
+gulp.task('_release:js', function () {
+    return gulp.src("src/www/js/main.js")
+        .pipe(requirejsOptimize({
+            mainConfigFile: 'require-config.js'
+        })).pipe(gulp.dest(destDir + "/js/"));
+});
+
+gulp.task('_release:css', function (done) {
+    gulp.src('./src/scss/base.scss')
+        .pipe(sass())
+        .pipe(cleanCSS())
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(gulp.dest(destDir + '/css/'))
+        .on('end', done);
+});
+
+
 
 
 
---
+++ require-config.js
@@ -0,0 +1,32 @@
+// 文件名称: require-config.js
+//
+// 创 建 人: fishYu
+// 创建日期: 2016/6/22 22:17
+// 描    述: require-config
+var requirejs={
+    baseUrl: "src/www/js",
+    shim: {
+        underscore: {
+            exports : '_'
+        },
+        backbone: {
+            deps : [
+                'underscore',
+                'jquery'
+            ],
+            exports : 'Backbone'
+        },
+        marionette: {
+            exports: 'Backbone.Marionette',
+            deps: ['jquery','backbone']
+        }
+	},
+	paths: {
+        jquery: 'vendor/jquery-3.0.0.min',
+        underscore: 'vendor/underscore',
+        backbone: 'vendor/backbone/backbone',
+        marionette : 'vendor/backbone/backbone.marionette',
+        text : 'vendor/text',
+        touchSlide : 'vendor/TouchSlide.1.1'
+	}
+}
