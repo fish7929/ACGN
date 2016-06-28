cdd77e12d2a7fe00e416df4e7d3ab5c70e5dc1fb
= <=>
2016-06-28 23:34:25 +0800

    提交发布页面

---
+++ src/www/js/module/publish/views/publishOptionView.js
@@ -0,0 +1,65 @@
+// 文件名称: publishView.js
+//
+// 创 建 人: zhao
+// 创建日期: 2016/6/26 17:00
+// 描    述: 发布页面
+define([
+    'common/base/base_view',
+    'text!module/publish/templates/publishOptionView.html',
+    'marionette',
+    'common/region/switch_view_region',
+    'common/views/loginBar'
+],function(BaseView, tpl, mn, SwitchViewRegion, LoginBarView){
+    return BaseView.extend({
+        id : "publishOptContainer",
+        template : _.template(tpl),
+
+        // key : selector
+        ui : {
+        },
+
+        //事件添加
+        events : {
+        },
+
+        regions : {
+            LoginBarRegion: {
+                el: ".publishOpt-loginBar-reg",
+                regionClass: SwitchViewRegion
+            }
+        },
+
+        /**初始化**/
+        initialize : function(){
+            var self = this;
+            self._loginBarView = new LoginBarView();
+        },
+
+        //在开始渲染模板前执行，此时当前page没有添加到document
+        onBeforeRender : function(){
+        },
+
+        //渲染完模板后执行,此时当前page没有添加到document
+        onRender : function(){
+        },
+
+        //页间动画已经完成，当前page已经加入到document
+        pageIn : function(){
+            var self = this;
+            self.regionShow();
+        },
+
+        regionShow : function(){
+            var self = this;
+            self.LoginBarRegion.show(self._loginBarView);
+        },
+
+        /**页面关闭时调用，此时不会销毁页面**/
+        close : function(){
+        },
+
+        //当页面销毁时触发
+        onDestroy : function(){
+        }
+    });
+});
