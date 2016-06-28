5ed4a45e4b5e380be70076cd1ce760018ec53b78
zhaozhigang <zhaozhigang@gli.cn>
2016-06-28 18:25:34 +0800

    修改内容

--- src/www/js/module/book/views/bookPreviewView.js
+++ src/www/js/module/book/views/bookPreviewView.js
@@ -7,20 +7,111 @@ define([
     'text!module/book/templates/bookPreviewView.html'
 ],function(tpl){
     var BookPreviewView = function(){
-        this._isShow = false;
-        this.$el = $(tpl);
-        this.el = this.$el.get(0);
+        var self = this;
+        self._isShow = false;
+        self.$el = $(tpl);
+        self.el = self.$el.get(0);
+        self._picArr = [];
+        self._picIndex = 0;
+
+        self._bookImage = self.$el.find(".previewImage");
+        self._nextBtn = self.$el.find(".previewRightBtn");
+        self._preBtn = self.$el.find(".previewLeftBtn");
+        self.show(["http://ac-syrskc2g.clouddn.com/95f2df33032a1574f0b7", "http://ac-syrskc2g.clouddn.com/1ab3c8b1a334770f5194"]);
     };
 
-    BookPreviewView.prototype.show = function(){
+    /**
+     * 显示要预览的书本
+     * @param arr 图片数组
+     */
+    BookPreviewView.prototype.show = function(arr){
         var self = this;
-        if(self._isShow){
-            return;
-        }
+        if(!arr || arr.length == 0) return;
+
+        if(self._isShow) return;
         self._isShow = true;
         if(self.el.parentNode){
             document.body.appendChild(self.el);
         }
+        self._picIndex = 0;
+        self._picArr = arr;
+        self.showPic();
+        self.bindEvent();
+    };
+
+    BookPreviewView.prototype.showPic = function(){
+        var self = this, url = self._picArr[self._picIndex];
+        if(!url) return;
+
+        var image = new Image();
+        image.onload = function(){
+            self.setImage(url, image.width, image.height);
+            image = null;
+        };
+        image.onerror = function(){
+            image = null;
+        };
+        image.src = url;
+
+        self.setBtnVi();
+    };
+
+    BookPreviewView.prototype.showNextPic = function(){
+        var self = this;
+        self._picIndex++;
+        self.showPic();
+    };
+
+    BookPreviewView.prototype.showPrePic = function(){
+        var self = this;
+        self._picIndex--;
+        self.showPic();
+    };
+
+    BookPreviewView.prototype.setImage = function(url, imageWidth, imageHeight){
+        var self = this;
+        self._bookImage.css({"width": imageWidth+"px", "height":+imageHeight+"px", "background":"url('"+url+"') no-repeat center"});
+        self.setPos();
+    };
+
+    BookPreviewView.prototype.setBtnVi = function(){
+        var self = this;
+        if(self._picIndex == 0){
+            self._preBtn.hide();
+        }else{
+            self._preBtn.show();
+        }
+
+        if(self._picIndex == self._picArr.length - 1){
+            self._nextBtn.hide();
+        }else{
+            self._nextBtn.show();
+        }
+    };
+
+    BookPreviewView.prototype.setPos = function(imageWidth, imageHeight){
+        var self = this;
+        var cssObj = {};
+        cssObj.height = imageHeight+"px";
+        self._nextBtn.css(cssObj);
+        self._preBtn.css(cssObj);
+    };
+
+    BookPreviewView.prototype.bindEvent = function(){
+        var self = this;
+        self._preBtn.bind("click", function(){
+            self.showPrePic();
+        });
+
+        self._nextBtn.bind("click", function(){
+            self.showNextPic();
+        });
+    };
+
+    BookPreviewView.prototype.removeEvent = function(){
+        var self = this;
+        self._preBtn.unbind("click");
+        self._nextBtn.unbind("click");
     };
 
     BookPreviewView.prototype.hide = function(){
@@ -32,6 +123,7 @@ define([
         if(!self.el.parentNode){
             self.el.remove();
         }
+        self.removeEvent();
     };
 
     return new BookPreviewView();
