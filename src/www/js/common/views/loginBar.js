765bef7f671870e5452ec2a0f164fa63af49ac36
fishYu <kingfish86@163.com>
2016-07-13 00:21:54 +0800

    先注释掉能用的设置

--- src/www/js/common/views/loginBar.js
+++ src/www/js/common/views/loginBar.js
@@ -196,8 +196,8 @@ define([
             e.stopPropagation();
             e.preventDefault();
             var self = this;
-//            MsgBox.alert("点击用户设置按钮");
-            ShowBox.setting();
+            MsgBox.toast("点击用户设置按钮");
+//            ShowBox.setting();
         },
         /*点击事件不可以重复点*/
         _checkMouseLock : function () {
